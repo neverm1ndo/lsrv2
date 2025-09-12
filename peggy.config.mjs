2981bbd3bdf02179a8c277f871c98a2f0e683bae
neverm1ndo <it@nmnd.ru>
2025-09-12 10:23:08 +0300

    ci: genereate parsers

--- peggy.config.mjs
+++
@@ -1,6 +0,0 @@
-export default {
-    input: "./src/parser/peg/logs.parser.pegjs",
-    output: "./src/parser/peg/logs.parser.js",
-    dts: true,
-    format: "es"
-};
---
+++ src/parser/llql/peg/peggy.config.mjs
@@ -0,0 +1,6 @@
+export default {
+    input: "./src/parser/llql/peg/llql.parser.pegjs",
+    output: "./src/parser/llql/peg/llql.parser.js",
+    dts: true,
+    format: "es"
+};
